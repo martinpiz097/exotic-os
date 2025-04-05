736583c8dbec7d89ba87de44c93d8e37a49f0823
Martin Pizarro <martinpiz097@gmail.com>
2025-04-05 14:54:19 -0300

    fix: correcciones varias al script para crear webapps

---
+++ setup/archlinux_postinstallation/desktop-webapps/apps/GitlabDesktop.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+"/home/martin/exotic-os/setup/archlinux_postinstallation/desktop-webapps/launch.sh" "https://gitlab.com/" "GitlabDesktop"
--- setup/archlinux_postinstallation/desktop-webapps/create_app.sh
+++ setup/archlinux_postinstallation/desktop-webapps/create_app.sh
@@ -10,7 +10,7 @@ APP_URL="$2"
 CLASS_NAME="${APP_NAME// /}"
 
 if [ -n "$3" ]; then
-  ICON_PATH="$3"
+  ICON_PATH="$(realpath "$3")"
 else
   ICON_PATH="web-browser"
 fi
@@ -23,8 +23,18 @@ if [ ! -f "$LAUNCH_SCRIPT_PATH" ]; then
   exit 1
 fi
 
+APPS_DIR="$SCRIPT_DIR/apps"
+mkdir -p "$APPS_DIR"
+
+APP_LAUNCHER_SH="$APPS_DIR/$CLASS_NAME.sh"
+cat > "$APP_LAUNCHER_SH" <<EOF
+#!/bin/bash
+"$LAUNCH_SCRIPT_PATH" "$APP_URL" "$CLASS_NAME"
+EOF
+chmod +x "$APP_LAUNCHER_SH"
+
 DESKTOP_DIR="$HOME/.local/share/applications"
-EXEC_COMMAND="\"$LAUNCH_SCRIPT_PATH\" \"$APP_URL\" \"$CLASS_NAME\""
+EXEC_COMMAND="\"$APP_LAUNCHER_SH\""
 FILE_NAME="${APP_NAME// /_}.desktop"
 DESKTOP_FILE_PATH="$DESKTOP_DIR/$FILE_NAME"
 
@@ -36,10 +46,11 @@ Name=$APP_NAME
 Exec=$EXEC_COMMAND
 Icon=$ICON_PATH
 Type=Application
-Categories=Network;WebBrowser;
+Categories=Development;
 StartupWMClass=$CLASS_NAME
 EOF
 
 chmod +x "$DESKTOP_FILE_PATH"
 
 echo "✅ App '$APP_NAME' creada en: $DESKTOP_FILE_PATH"
+echo "📄 Lanzador generado en: $APP_LAUNCHER_SH"
